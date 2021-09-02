b2c6d12f78154ed1684f9b7e80bbdf2c62229011
VictorReisSp <31421139+VictorReisSp@users.noreply.github.com>
2021-09-02 20:12:01 -0300

    Update modulo.mjs

--- modulo.mjs
+++ modulo.mjs
@@ -1,11 +1,9 @@
-export {conteudoModulo, umaSominhaAi};
+export {conteudoModulo};
 
 const conteudoModulo = {
     'nome' : 'Victor Reis',
     'idade' : 21,
     'prazeres' : 'console sem nenhum erro',
     'desprazeres' : 'CORS',
-
 }
 
-const umaSominhaAi = 2+2;
